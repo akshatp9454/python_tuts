9c2942532ae8969043e6002f19aa61392f2eec7f
Akshat Verma <akshatp9454@gmail.com>
2021-07-28 14:55:10 +0530

    added python tut 6

---
+++ tut6.py
@@ -0,0 +1,18 @@
+# single line comments
+''' mlti
+line
+comeents '''
+'''print("line1 #this generates an error
+line 2") '''
+
+print('''line 1 
+line 2''')
+
+print("first text",end="")
+print(" second text")
+
+print("first text again"," second text again",end="")
+
+# \n is an escape character, to ignore it type : \\n
+print('H\narry')
+print("H\\narry")
